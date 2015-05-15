d16bde3427c90e1105b4e988a7afe55daa148ba8
Nathan Menge <mengen@upmc.edu>
2015-05-15 16:03:02 -0400

    Enhanced dictionAerie clss to use Hashmap.  Thanks again Gilli.

--- src/com/example/dictionAerie.java
+++ src/com/example/dictionAerie.java
@@ -1,26 +1,21 @@
 package com.example;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Nate on 5/15/15.
  *
  * This is an implementation of a dictionary class, it's pretty high level, like up with the Eagles in their Aery.
  */
 public class dictionAerie implements wordDict{
-    String[] words;
+    Set<String> words;
 
-    public dictionAerie(String[] words) {
-        this.words = words;
-    }
+    public dictionAerie(String[] wordsArray) { words = new HashSet<>(Arrays.asList(wordsArray)); }
 
     @Override
     public boolean checkWord(String checking) {
-        boolean result = false;
-        for(String word: words) {
-            if(word.compareToIgnoreCase(checking) == 0) {
-                result = true;
-                break;
-            }
-        }
-        return result;
+        return words.contains(checking);
     }
 }
